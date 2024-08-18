b595f4a0067d98af5240f9b0ff4b1e8f18882c68
Subham <subhamoffi@gmail.com>
2024-08-18 18:46:15 +0530

    Random Quotes are generated via JS

---
+++ script.js
@@ -0,0 +1,25 @@
+getQuote();
+
+async function getQuote() {
+    let mainNum = randomNumGenerator();
+
+    let quote = await fetch(`https://dummyjson.com/quotes/${mainNum}`);
+    let quoteData = await quote.json();
+
+    console.log(quoteData);
+}
+
+function randomNumGenerator() {
+    let randomNum1 = Math.round(Math.random() * 100);
+    let randomNum2 = Math.round(Math.random() * 100);
+        
+    if(randomNum1 * randomNum2 <= 1454){
+        let mainNum = randomNum1 * randomNum2;
+        return mainNum;
+    }
+    else{
+        let mainNum = randomNumGenerator();
+        return mainNum
+    }
+
+}
